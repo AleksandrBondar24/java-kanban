585cf24c6172b1dbbb163af50e049a055e6a40ee
Aleksandr Bondar <alexbondar.b@yandex.ru>
2022-10-27 19:33:30 +0300

    "feat: добавить проверку проведенного рефокторинга и работы новой функциональности по отображению последних просмотренных пользователем задач.  "

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
-import domain.EpicTask;
-import domain.Status;
-import domain.SubTask;
-import domain.Task;
 import manager.TaskManager;
+import task.EpicTask;
+import task.Status;
+import task.SubTask;
+import task.Task;
+import utils.Managers;
 
 public class Main {
 
     public static void main(String[] args) {
         // Тесты:
-        TaskManager manager = new TaskManager();
+        TaskManager manager = Managers.getDefault();
+
         Task task = new Task("Уборка", Status.NEW, "Помыть полы и пропылесосить");
         Task task1 = new Task("Покупка", Status.NEW, "Купить хлеб и молоко");
         EpicTask task2 = new EpicTask("Построить дом", "Кирпичный дом с 4 комнатами");
@@ -19,6 +21,12 @@ public class Main {
         EpicTask task5 = new EpicTask("Сьездить в отпуск", "Посетить одно из чудес света");
         SubTask task6 = new SubTask("Купить путевки", Status.NEW,
                 "Успеть купить горящие путевки", 6);
+        SubTask task7 = new SubTask("Построить крышу", Status.NEW, "Цвет будет красным", 3);
+        SubTask task8 = new SubTask("Установить двери", Status.NEW,
+                "Металлические девери", 3);
+        Task task9 = new Task("Получить 10 лвл", Status.NEW, "В течении дня");
+        Task task10 = new Task("Заточить оружие", Status.NEW, "За 3 попытки");
+        Task task11 = new Task("Пройти данж", Status.NEW, "Без смертей");
         manager.addTask(task);
         manager.addTask(task1);
         manager.addEpicTask(task2);
@@ -26,36 +34,52 @@ public class Main {
         manager.addSubTask(task4);
         manager.addEpicTask(task5);
         manager.addSubTask(task6);
+        manager.addSubTask(task7);
+        manager.addSubTask(task8);
+        manager.addTask(task9);
+        manager.addTask(task10);
+        manager.addTask(task11);
         System.out.println(manager.getListTasks());
         System.out.println(manager.getListEpicTasks());
         System.out.println(manager.getListSubTasks());
-        Task task7 = new Task(1, "Уборка", Status.IN_PROGRESS,
+        Task task13 = new Task(1, "Уборка", Status.IN_PROGRESS,
                 "Помыть полы и пропылесосить");
-        Task task8 = new Task(2, "Покупка", Status.IN_PROGRESS,
+        Task task14 = new Task(2, "Покупка", Status.IN_PROGRESS,
                 "Купить хлеб и молоко");
-        EpicTask task9 = new EpicTask(3, "Построить дом", "Кирпичный дом с 3 комнатами");
-        SubTask task10 = new SubTask(4, "Залить фундамент", Status.IN_PROGRESS,
+        EpicTask task15 = new EpicTask(3, "Построить дом", "Кирпичный дом с 3 комнатами");
+        SubTask task16 = new SubTask(4, "Залить фундамент", Status.IN_PROGRESS,
                 "Глубина 2 метра", 3);
-        SubTask task11 = new SubTask(5, "Выложить коробку", Status.DONE,
+        SubTask task17 = new SubTask(5, "Выложить коробку", Status.DONE,
                 "Использовать белый кирпич", 3);
-        EpicTask task12 = new EpicTask(6, "Сьездить в отпуск", "Посетить все чудеса света");
-        SubTask task13 = new SubTask(7, "Купить путевки", Status.IN_PROGRESS,
+        EpicTask task18 = new EpicTask(6, "Сьездить в отпуск", "Посетить все чудеса света");
+        SubTask task19 = new SubTask(7, "Купить путевки", Status.IN_PROGRESS,
                 "Успеть купить горящие путевки", 6);
-        manager.updateTasks(task7);
-        manager.updateTasks(task8);
-        manager.updateEpicTasks(task9);
-        manager.updateSubTasks(task10);
-        manager.updateSubTasks(task11);
-        manager.updateEpicTasks(task12);
-        manager.updateSubTasks(task13);
+        manager.updateTasks(task19);
+        manager.updateTasks(task14);
+        manager.updateEpicTasks(task15);
+        manager.updateSubTasks(task16);
+        manager.updateSubTasks(task17);
+        manager.updateEpicTasks(task18);
+        manager.updateSubTasks(task19);
         System.out.println(manager.getListTasks());
         System.out.println(manager.getListEpicTasks());
-        System.out.println(manager.getListSubTasks());
-        manager.removeTask(task8);
-        manager.removeEpicTask(task12);
-        manager.removeSubTask(task10);
-        System.out.println(manager.getListTasks());
-        System.out.println(manager.getListEpicTasks());
-        System.out.println(manager.getListSubTasks());
+        System.out.println(manager.getListSubTasks() + "\n");
+
+        // Новые тесты:
+
+        manager.getTask(task13);
+        manager.getTask(task14);
+        manager.getTask(task9);
+        manager.getTask(task10);
+        manager.getTask(task11);
+        manager.getEpicTask(task15);
+        manager.getEpicTask(task18);
+        manager.getSubTask(task7);
+        manager.getSubTask(task8);
+        manager.getSubTask(task16);
+        manager.getSubTask(task17);
+        System.out.println(manager.getHistory());
+        manager.getSubTask(task17);
+        System.out.println(manager.getHistory());
     }
 }
