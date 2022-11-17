58208dffd09ac451cb32486ff18ef948d1e880ac
Aleksandr Bondar <alexbondar.b@yandex.ru>
2022-11-17 15:47:52 +0300

    "fix: создать HashMap historyTask.Реализовать CustomLinkedList - поля и класс Node.Реализовать метод remove.Создать и реализовать методы linkLast,getTask."

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -2,22 +2,86 @@ package manager;
 
 import task.Task;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final LinkedList<Task> historys = new LinkedList<>();
+    private final Map<Integer, Node<Task>> historyTask = new HashMap<>();
+    public Node<Task> first;
+    public Node<Task> last;
+    public int size = 0;
 
     @Override
     public List<Task> getHistory() {
-        return new LinkedList<>(historys);
+        return getTasks();
     }
 
     @Override
     public void add(Task task) {
-        if (historys.size() == 10) {
-            historys.removeFirst();
+        remove(task.getIdTask());
+        linkLast(task);
+        historyTask.put(task.getIdTask(), last);
+    }
+
+    @Override
+    public void remove(int id) {
+        Node<Task> node = historyTask.get(id);
+        if (node == null) {
+            return;
         }
-        historys.add(task);
+        final Node<Task> next = node.next;
+        final Node<Task> prev = node.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            node.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            node.next = null;
+        }
+        node.task = null;
+        size--;
+    }
+
+    private void linkLast(Task task) {
+        final Node<Task> l = last;
+        final Node<Task> newNode = new Node<>(l, task, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+
+    private List<Task> getTasks() {
+        List<Task> list = new ArrayList<>();
+        Node<Task> node = first;
+        for (int i = 0; i < size; i++) {
+            if (node.task == null) {
+                throw new NoSuchElementException();
+            } else {
+                list.add(node.task);
+            }
+            node = node.next;
+        }
+        return list;
+    }
+
+    static class Node<Task> {
+        public Node<Task> next;
+        public Node<Task> prev;
+        public Task task;
+
+        public Node(Node<Task> prev, Task task, Node<Task> next) {
+            this.next = next;
+            this.prev = prev;
+            this.task = task;
+        }
     }
 }
